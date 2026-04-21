a6dc15a986223452fa6e2db0fa06715704ca8050
Lazuli Costa <contato.lazuli.costa@gmail.com>
2026-04-21 13:08:44 -0300

    Create dataloader for CSV files

---
+++ src/loader.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+class CsvDataloader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.dataframe = pd.read_csv(file_path)
+
+    def load_xy(self, target_column):
+        X = self.dataframe.drop(columns=[target_column])
+        y = self.dataframe[target_column]
+        return X, y
