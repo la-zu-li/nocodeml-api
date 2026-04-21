b94e40face5088d5f3de30614739eb4ebea0de6f
Lazuli Costa <contato.lazuli.costa@gmail.com>
2026-04-21 14:27:01 -0300

    Implement saving pipeline for first model: "LinearRegression"

--- src/models.py
+++ src/models.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
+from uuid import uuid4
 
 import joblib
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeClassifier
+
+from .env import CONFIG_PATH
 
 
 class Model(ABC):
@@ -19,6 +22,7 @@ class Model(ABC):
 class LinearRegressionModel(Model):
     def __init__(self):
         self.model = LinearRegression()
+        self.id = uuid4()
 
     def train(self, X, y):
         self.model.fit(X, y)
@@ -26,8 +30,18 @@ class LinearRegressionModel(Model):
     def predict(self, X):
         prediction = self.model.predict(X)
 
-    def dump(self, file_path):
+    def dump(self):
+        folder_path = CONFIG_PATH / "models"
+        folder_path.mkdir(exist_ok=True, parents=True)
+
+        filename = f"linear_regression_{self.id}.pkl"
+        file_path = folder_path / filename
         joblib.dump(self.model, file_path)
+        return file_path
+
+    def save(self):
+        file_path = self.dump()
+        return {"id": self.id, "model_path": file_path}
 
 
 class DecisionTreeModel(Model):
